18176a0b29ef501e549d9b4a1b8e30763adf25ec
datalurkur <datalurkur@gmail.com>
2012-11-07 23:05:15 -0800

    Comments and cleanup

--- root_state.rb
+++
@@ -1,2 +0,0 @@
-class RootState
-end
--- util.rb
+++
@@ -1,8 +0,0 @@
-class Object
-    def defer_to(member, method)
-        methods_to_defer = (Array === method) ? method : [method]
-        methods_to_defer.each { |i|
-            define_method(i) { |*args| instance_variable_get("@#{member}").send(i,*args) }
-        }
-    end
-end
