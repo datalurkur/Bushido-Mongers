e911a9b1246d3c4a5c2b66282db0a8e15e5646cb
datalurkur <datalurkur@gmail.com>
2012-11-14 11:37:10 -0800

    Removes old file locations, which git apparently isn't smart enough to just move.

--- states/lobby_state.rb
+++
@@ -1,8 +0,0 @@
-require 'state'
-
-class LobbyState < State
-    def initialize(client)
-        super(client)
-        @client.send_to_client(Message.new(:notify, {:text=>"You have entered the lobby"}))
-    end
-end
