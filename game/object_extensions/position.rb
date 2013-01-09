b8f72520263bf5d283797bfe5862b59b01ca8f38
Paul <paulwuersig@gmail.com>
2013-01-09 15:20:14 -0600

    Re-unify move and position; it's just position now

--- game/object_extensions/position.rb
+++ game/object_extensions/position.rb
@@ -24,4 +24,16 @@ module Position
         @position.remove_object(self)
         @position = nil
     end
+
+    def move(direction)
+        raise Exception, "Position uninitialized; is #{self.name} positionable?" if @position.nil?
+
+        # This method raises an exception if the direction is invalid, so no need to check it
+        new_position = @position.connected_leaf(direction)
+
+        Log.debug("#{monicker} moves from #{@position.name} to #{new_position.name}")
+        @position.remove_object(self)
+        @position = new_position
+        @position.add_object(self)
+    end
 end
