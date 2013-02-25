d2d5b7b77492a692d846097e2d16164f1740d6cd
datalurkur <datalurkur@gmail.com>
2013-02-25 14:11:29 -0800

    Fixes broken unit tests

--- test/fake.rb
+++ test/fake.rb
@@ -21,6 +21,9 @@ unless Object.const_defined?("Message")
             def register_listener(core, klass, obj); end
             def unregister_listener(core, klass, obj); end
             def dispatch(core, type, args={}); end
+            def dispatch_positional(core, locations, type, args={}); end
+            def change_listener_position(core, listener, position, old_position); end
+            def clear_listener_position(core, listener, position); end
         end
     end
 end
