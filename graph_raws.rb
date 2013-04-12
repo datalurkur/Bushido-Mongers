9437aa3ed56f109b1914efe6fa71d4f9b5684bf2
datalurkur <datalurkur@gmail.com>
2013-04-12 09:42:28 -0700

    Adds the raw grapher tool

---
+++ graph_raws.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'graphviz'
+
+require 'raws/bushido_object'
+require 'raws/db'
+
+Log.setup("Main", "grapher")
+
+group = "default"
+grapher = GraphViz.new(:G, :type => :digraph)
+begin
+    ObjectRawParser.load_db(group, grapher)
+rescue Exception => e
+    Log.warning(["Failed to finish parsing objects", e.message, e.backtrace])
+end
+grapher.output(:png => "#{group}_graph.png")
