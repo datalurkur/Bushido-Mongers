7fcae21df67016f4f737000c207b8691f1f05ee0
Paul Wuersig <paulwuersig@gmail.com>
2013-11-15 16:16:24 -0600

    Fix run_debug.

--- run_debug.rb
+++ run_debug.rb
@@ -1,3 +1,14 @@
+require './world/factories'
 $TestWorldFactory = FantasmTestWorldFactory
 
 require './run_local'
+
+if __FILE__ == $0
+    $client.start
+
+    while $client.running?
+        sleep 10
+    end
+
+    $server.stop
+end
