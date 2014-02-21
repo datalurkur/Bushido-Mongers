9483a7eb29df8080f943e883ec89d82b5fecafe4
datalurkur <datalurkur@gmail.com>
2014-02-20 16:49:15 -0800

    Adds a base for an ncurses raw editor

--- build_tools.sh
+++ build_tools.sh
@@ -2,3 +2,6 @@
 pushd tools/raw_editor
 ./build.sh
 popd
+pushd tools/raw_editor_ncurses
+./build.sh
+popd
---
+++ tools/raw_editor_ncurses/build.sh
@@ -0,0 +1,1 @@
+g++ -W -Wall -Wextra -pedantic -std=c++11 -ggdb -I ../.. *.cpp -lncurses -o ../../edit_raws_ncurses
